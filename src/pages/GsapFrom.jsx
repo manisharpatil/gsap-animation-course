176e337667ac0473b79b6983edfca53eccf150a7
Manisha Patil <manisharpatil6@gmail.com>
2025-06-21 16:29:11 +0530

    Implement the GsapTo, GsapFrom, GsapFromTo methos

--- src/pages/GsapFrom.jsx
+++ src/pages/GsapFrom.jsx
@@ -1,5 +1,18 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapFrom = () => {
-  // TODO: Implement the gsap.from() method
+  useGSAP(() => {
+    gsap.from("#green-box", {
+      x: 250,
+      rotation: 360,
+      borderRadius: "100%",
+      duration: 2,
+      ease: "power1.inOut",
+      repeat: -1,
+      yoyo: true
+    });
+  });
 
   return (
     <main>
